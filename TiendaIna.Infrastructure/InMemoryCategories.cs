4d3cd9d6cea3c3fb0ba288722cb487d81e6ba10d
Rebeca Angelico <rebecaangelico07@gmail.com>
2025-08-21 19:46:19 -0300

    + inMemory Categories

---
+++ TiendaIna.Infrastructure/InMemoryCategories.cs
@@ -0,0 +1,45 @@
+﻿using TiendaIna.Core.Entities;
+
+namespace TiendaIna.Infrastructure {
+    public interface IInMemoryCategories : IList<Category> { }
+
+    public class InMemoryCategories : List<Category>, IInMemoryCategories {
+        public InMemoryCategories() {
+            this.Clear();
+            this.AddRange([
+              new Category {
+                Id = 1,
+               Name = "Perfumes Arabes",
+              },
+              new Category {
+                Id = 2,
+               Name = "Perfumes Masculinos",
+              },
+              new Category {
+                Id = 3,
+               Name = "Perfumes Femeninos",
+              },
+              new Category {
+                Id = 4,
+               Name = "Perfumes Unisex",
+              },
+              new Category {
+                Id = 5,
+               Name = "Perfumes Infantiles",
+              },
+              new Category {
+                Id = 6,
+               Name = "Perfumes Franceses",
+              },
+              new Category {
+                Id = 7,
+               Name = "Productos Capilares",
+              },
+              new Category {
+                Id = 8,
+               Name = "Ofertas",
+              },
+        ]);
+        }
+    }
+}
