9e7deff7a931429f52b6f917c53c8b6735ac75f8
Rebeca Angelico <rebecaangelico07@gmail.com>
2025-08-26 19:31:56 -0300

    estable funcionando

--- TiendaIna.Admin.Blazor/Components/Pages/Shared/Components/EditNumber.razor.cs
+++ TiendaIna.Admin.Blazor/Components/Pages/Shared/Components/EditNumber.razor.cs
@@ -9,63 +9,22 @@ namespace TiendaIna.Admin.Blazor.Components.Pages.Shared.Components {
         #endregion
 
         #region Parameters
-        [Parameter] public TValue Value { get; set; }
+        [Parameter] public TValue? Value { get; set; }
         [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
-        [Parameter] public string Placeholder { get; set; } = "Ingrese un monto";
+        [Parameter] public string? Placeholder { get; set; }
         [Parameter] public bool Disabled { get; set; } = false;
         [Parameter] public string CssClass { get; set; } = "form-control";
         [Parameter] public int Decimals { get; set; } = 2;
         [Parameter] public string CurrencySymbol { get; set; } = "$";
-        [Parameter] public decimal StepDecimal { get; set; } = 0.01m;
-        [Parameter] public TValue MinValue { get; set; }
-        [Parameter] public TValue MaxValue { get; set; }
+        [Parameter] public decimal? Min { get; set; }
+        [Parameter] public decimal? Max { get; set; }
         [Parameter] public bool ShowUpDown { get; set; } = true;
+        [Parameter] public TValue? StepValue { get; set; }
         #endregion
 
-        #region Methods
-        private decimal GetMinValue() {
-            return MinValue Convert.ToDecimal();
-        }
-
-        private TValue GetMaxValue() {
-            return MaxValue;
-        }
-
-        private Type GetUnderlyingType() {
-            var type = typeof(TValue);
-
-            // Si es nullable, obtener el tipo subyacente
-            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>)) {
-                return Nullable.GetUnderlyingType(type);
-            }
-
-            return type;
-        }
-
-        private decimal GetStepValue() {
-            var underlyingType = GetUnderlyingType();
-
-            // Si es int, el step debería ser 1
-            if (underlyingType == typeof(int) || underlyingType == typeof(long)) {
-                return 1m;
-            }
-
-            return StepDecimal;
-        }
-
-        private string GetFormatString() {
-            var underlyingType = GetUnderlyingType();
-
-            // Solo aplicar formato de moneda para tipos decimales/float
-            if (underlyingType == typeof(decimal) ||
-                underlyingType == typeof(double) ||
-                underlyingType == typeof(float)) {
-                return CurrencyFormat;
-            }
-
-            // Para enteros, formato sin decimales
-            return "N0";
-        }
+        #region read-only properties
+        public string Format => "";
+        public string Step => StepValue?.ToString() ?? "1";
         #endregion
     }
 }
