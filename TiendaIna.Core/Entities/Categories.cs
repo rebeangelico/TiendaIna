e1352fc6716ae2560f7b2a9f5b1957db14fa51ae
Rebeca Angelico <rebecaangelico07@gmail.com>
2025-09-19 20:23:27 -0300

    paquete RepoDb instalado

--- TiendaIna.Core/Entities/Categories.cs
+++ TiendaIna.Core/Entities/Categories.cs
@@ -6,16 +6,8 @@ namespace TiendaIna.Core.Entities {
     public class Category : IEntity<int> {
         public int Id { get; set; }
         public string? Name { get; set; }
-
-        // Categorias padre
         public int? ParentCategoryId { get; set; }
-        public virtual Category ParentCategory { get; set; }
 
-        // Colección de categorías hijas
-        public virtual ICollection<Category>? ChildCategories { get; set; }
-
-        //coleccion productos por categoria
-        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
         public Category(CategoryModel model) {
             Id = model.Id;
             Name = model.Name;
