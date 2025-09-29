959e26ca8e67d24567a4d8a09f29badba42f4dec
Rebeca Angelico <rebecaangelico07@gmail.com>
2025-09-29 17:46:14 -0300

    Image entity Mejorado

--- TiendaIna.Core/Entities/Image.cs
+++ TiendaIna.Core/Entities/Image.cs
@@ -5,10 +5,12 @@ namespace TiendaIna.Core.Entities {
     public class Image : IEntity<int> {
         public int Id { get; set; } 
         public string Url { get; set; }
+        public string CdnUrl { get; set; }
 
         public Image(ImageModel model) {
             Id = model.Id;
             Url = model.Url;
+            CdnUrl = model.CdnUrl;
         }
         public Image() {
             
--- TiendaIna.Core/Models/ImageModel.cs
+++ TiendaIna.Core/Models/ImageModel.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TiendaIna.Core.Entities;
+﻿using TiendaIna.Core.Entities;
 
 namespace TiendaIna.Core.Models {
     public class ImageModel {
         public int Id { get; set; }
         public string Url { get; set; }
+        public string CdnUrl { get; set; }
 
         public ImageModel() { }
 
         public ImageModel(Image brand) {
             Id = brand.Id;
             Url = brand.Url;
+            CdnUrl = brand.CdnUrl;
         }
 
     }
