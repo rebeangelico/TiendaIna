ecb3865509ebdaf683a32d7f6bb8519fefb5f458
Rebeca Angelico <rebecaangelico07@gmail.com>
2025-10-03 14:55:17 -0300

    images RepoDb

---
+++ TiendaIna.Infrastructure/Repos/ImagesDbRepo.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Options;
+using TiendaIna.Core.Entities;
+using TiendaIna.Core.Models;
+using TiendaIna.Core.Repos;
+
+namespace TiendaIna.Infrastructure.Repos;
+
+public class ImagesDbRepo : DbRepoBase<Image, int>, IImagesRepo {
+    public ImagesDbRepo(IOptions<AppSettings> appSettings) : base(appSettings) { }
+}
+
