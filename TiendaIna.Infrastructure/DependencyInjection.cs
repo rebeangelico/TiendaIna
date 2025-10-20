9dbf60a9df5b88426e4ebf24c6122929fef5c543
Diego Farias <diego.farias_contractor@jmfamily.com>
2025-10-20 20:38:27 -0300

    Improvements

--- TiendaIna.Infrastructure/DependencyInjection.cs
+++ TiendaIna.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TiendaIna.Core.Models;
 using TiendaIna.Core.Repos;
 using TiendaIna.Core.Services;
 using TiendaIna.Infrastructure.DataStore;
@@ -16,19 +17,28 @@ namespace TiendaIna.Core {
             services.AddScoped<IImagesService, ImagesService>();
             services.AddScoped<IProductImagesService, ProductImagesService>();
 
-            services.AddScoped<IProductsRepo, ProductsInMemoryRepo>();
-            services.AddScoped<IProductImagesRepo, ProductImagesInMemoryRepo>();
-            services.AddScoped<ICategoriesRepo, CategoriesInMemoryRepo>();
-            services.AddScoped<IBrandsRepo, BrandsInMemoryRepo>();
-            services.AddScoped<IImagesRepo, ImagesInMemoryRepo>();
+            var inMemoryRepos = bool.Parse(configuration.GetSection("InMemoryRepos").Value ?? false.ToString());
+            if (inMemoryRepos) {
+                services.AddScoped<IProductsRepo, ProductsInMemoryRepo>();
+                services.AddScoped<IProductImagesRepo, ProductImagesInMemoryRepo>();
+                services.AddScoped<ICategoriesRepo, CategoriesInMemoryRepo>();
+                services.AddScoped<IBrandsRepo, BrandsInMemoryRepo>();
+                services.AddScoped<IImagesRepo, ImagesInMemoryRepo>();
 
-            //in-memory stores (only for local testing)
-            services.AddSingleton<IInMemoryProductsStore, InMemoryProductsStore>();
-            services.AddSingleton<IInMemoryProductsCategoriesStore, InMemoryProductsCategoriesStore>();
-            services.AddSingleton<IInMemoryProductImagesStore, InMemoryProductImagesStore>();
-            services.AddSingleton<IInMemoryCategoriesStore, InMemoryCategoriesStore>();
-            services.AddSingleton<IInMemoryBrandsStore, InMemoryBrandsStore>();
-            services.AddSingleton<IInMemoryImagesStore, InMemoryImagesStore>();
+                //in-memory stores (only for local testing)
+                services.AddSingleton<IInMemoryProductsStore, InMemoryProductsStore>();
+                services.AddSingleton<IInMemoryProductsCategoriesStore, InMemoryProductsCategoriesStore>();
+                services.AddSingleton<IInMemoryProductImagesStore, InMemoryProductImagesStore>();
+                services.AddSingleton<IInMemoryCategoriesStore, InMemoryCategoriesStore>();
+                services.AddSingleton<IInMemoryBrandsStore, InMemoryBrandsStore>();
+                services.AddSingleton<IInMemoryImagesStore, InMemoryImagesStore>();
+            } else {
+                services.AddScoped<IProductsRepo, ProductsDbRepo>();
+                services.AddScoped<IProductImagesRepo, ProductImagesDbRepo>();
+                services.AddScoped<ICategoriesRepo, CategoriesDbRepo>();
+                services.AddScoped<IBrandsRepo, BrandsDbRepo>();
+                services.AddScoped<IImagesRepo, ImagesDbRepo>();
+            }
         }
     }
 }
