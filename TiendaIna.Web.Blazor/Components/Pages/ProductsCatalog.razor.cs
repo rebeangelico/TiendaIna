052f216f932c56933e962a08183fbf6e4052a2c9
Rebeca Angelico <rebecaangelico07@gmail.com>
2025-10-28 23:46:47 -0300

    wip

--- TiendaIna.Web.Blazor/Components/Pages/ProductsCatalog.razor.cs
+++ TiendaIna.Web.Blazor/Components/Pages/ProductsCatalog.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Radzen;
+using TiendaIna.Core.Entities;
+using TiendaIna.Core.Extensions;
 using TiendaIna.Core.Models;
 using TiendaIna.Core.Services;
 
@@ -7,25 +9,28 @@ namespace TiendaIna.Web.Blazor.Components.Pages;
 public partial class ProductsCatalog : ComponentBase {
     #region fields
     private readonly IProductsService _productsService;
-    private readonly ICategoriesService _categoriesService;
     private readonly DialogService _dialogService;
     private readonly NotificationService _notificationService;
     #endregion
 
     #region properties
 
-    public List<ProductModel>? Products { get; set; }
-    private IEnumerable<ProductModel> pagedProducts = [];
+    public List<ProductModel>? Products { get; set;}
+    public IEnumerable<ProductModel>? FilteredProducts { get; set; }
+    public IEnumerable<ProductModel> PagedProducts = [];
+    public int? BrandId;
+    public int? CategoryId;
 
-    private int pageSize = 9;
-    private int currentPage = 0;
+    private List<BrandModel>? Brands { get; set; }
+
+    private int _pageSize = 9;
+    private int _currentPage = 0;
     public bool IsLoading { get; set; } = false;
     #endregion
 
     #region constructors
-    public ProductsCatalog(IProductsService productsService, ICategoriesService categoriesService, DialogService dialogService, NotificationService notificationService) : base() {
+    public ProductsCatalog(IProductsService productsService, DialogService dialogService, NotificationService notificationService) : base() {
         _productsService = productsService ?? throw new ArgumentNullException(nameof(productsService));
-        _categoriesService = categoriesService ?? throw new ArgumentNullException(nameof(categoriesService));
         _dialogService = dialogService ?? throw new ArgumentNullException(nameof(dialogService));
         _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
     }
@@ -33,35 +38,70 @@ public partial class ProductsCatalog : ComponentBase {
 
     #region overriden methods
     protected override async Task OnInitializedAsync() {
-        try {
-            IsLoading = true;
-            Products = await _productsService.Get();
-            UpdatePagedProducts();
-            StateHasChanged();
-        } catch (Exception ex) {
-            NotifyError("Error al cargar los productos", ex);
-        } finally {
-            IsLoading = false;
-        }
+        IsLoading = true;
+        Products = await _productsService.Get();
+        SetFiltersFromUrl();
+        ApplyFilters();
+        StateHasChanged();
+        IsLoading = false;
+        await base.OnInitializedAsync();
     }
     #endregion
+
     #region methods
-    public void AddToCart(int productId, int amount) {
-    
-    
+    public void AddToCart(int productId, int amount) {} //implementar
+    private void ApplyFilters() {
+        IsLoading = true;
+        FilteredProducts = Products.DeepClone();
+        if (CategoryId is not null)
+            FilteredProducts = FilteredProducts?.Where(p => p.Categories != null && p.Categories.Any(c => c.Id == CategoryId));
+        if (BrandId is not null)
+            FilteredProducts = FilteredProducts?.Where(p => p.BrandId == BrandId);
+
+        _currentPage = 0;
+        UpdatePagedProducts();
+        IsLoading = false;
+        StateHasChanged();
     }
     private void OnPageChanged(PagerEventArgs args) {
-        currentPage = args.PageIndex;
+        _currentPage = args.PageIndex;
         UpdatePagedProducts();
     }
 
     private void UpdatePagedProducts() {
-        pagedProducts = Products!
+        PagedProducts = FilteredProducts!
             .Where(p => p != null)
-            .Skip(currentPage * pageSize)
-            .Take(pageSize);
+            .Skip(_currentPage * _pageSize)
+            .Take(_pageSize);
     }
 
+    private void FilterByBrand(int? brandId) {
+        ResetFilters();
+        BrandId = brandId;
+        ApplyFilters();
+    }
+
+    private void FilterByCategory(int? categoryId) {
+        ResetFilters();
+        CategoryId = categoryId;
+        ApplyFilters();
+    }
+
+    private void ResetFilters() {
+        BrandId = null;
+        CategoryId = null;
+        StateHasChanged();
+    }
+
+    private void SetFiltersFromUrl() {
+        var uri = new Uri(NavigationManager.Uri);
+        var queryParams = System.Web.HttpUtility.ParseQueryString(uri.Query);
+
+        if (int.TryParse(queryParams["brandId"], out var brandId))
+            BrandId = brandId;
+        if (int.TryParse(queryParams["categoryId"], out var categoryId))
+            CategoryId = categoryId;
+    }
     #endregion
 
     #region Helpers
